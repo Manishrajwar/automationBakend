5289510b059fa3cd3f8f2b23d725594fe0789c0d
Manishrajwar <manishsinghrajwar80@gmail.com>
2024-07-18 10:19:19 +0530

    done

--- controllers/auth.js
+++ controllers/auth.js
@@ -94,8 +94,8 @@ exports.start = async(req , res)=>{
   
     const browser = await puppeteer.launch({
       headless: false,
-      // executablePath: '/usr/bin/google-chrome-stable',
-      args: ["--disable-notifications", "--start-maximized" , "--no-sandbox", "--disable-setuid-sandbox"],
+      executablePath: '/path/to/Chrome',
+            args: ["--disable-notifications", "--start-maximized" , "--no-sandbox", "--disable-setuid-sandbox"],
       defaultViewport: null,
       slowMo: true,
     });
